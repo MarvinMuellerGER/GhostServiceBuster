e261b2cc867e438ef3c5e46a26af6eee4f9705f8
MarvinMuellerGER <113536472+MarvinMuellerGER@users.noreply.github.com>
2025-12-02 20:31:06 +0100

    use file keyword

--- src/Extensions/MS/Filter/ServiceProviderUsageFilter.cs
+++ src/Extensions/MS/Filter/ServiceProviderUsageFilter.cs
@@ -8,21 +8,12 @@ namespace GhostServiceBuster.MS.Filter;
 
 file sealed class ServiceProviderUsageFilter : IServiceInfoFilter
 {
-    private FrozenSet<Type>? _typesResolvedByServiceProvider;
+    private FrozenSet<Type> TypesResolvedByServiceProvider =>
+        field ??= AppDomain.CurrentDomain.GetAssemblies().SelectMany(a =>
+                a.GetCustomAttributes<TypesResolvedByServiceProviderAttribute>().SelectMany(ca => ca.Types))
+            .ToFrozenSet();
 
     public bool IsIndividual => true;
-
-    private FrozenSet<Type> TypesResolvedByServiceProvider
-    {
-        get
-        {
-            _typesResolvedByServiceProvider ??= AppDomain.CurrentDomain.GetAssemblies().SelectMany(a =>
-                    a.GetCustomAttributes<TypesResolvedByServiceProviderAttribute>().SelectMany(ca => ca.Types))
-                .ToFrozenSet();
-
-            return _typesResolvedByServiceProvider;
-        }
-    }
 
     public ServiceInfoSet GetFilteredServices(ServiceInfoSet serviceInfos) =>
         serviceInfos.Where(s => TypesResolvedByServiceProvider.Contains(s.ServiceType));
