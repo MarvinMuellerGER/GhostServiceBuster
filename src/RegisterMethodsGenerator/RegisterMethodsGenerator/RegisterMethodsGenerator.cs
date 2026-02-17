22042240b45d9d23ef5ff36b369213b528162236
MarvinMuellerGER <113536472+MarvinMuellerGER@users.noreply.github.com>
2026-02-18 00:33:16 +0100

    remove package version from RegisterMethodsGenerator

--- src/RegisterMethodsGenerator/RegisterMethodsGenerator/RegisterMethodsGenerator.cs
+++ src/RegisterMethodsGenerator/RegisterMethodsGenerator/RegisterMethodsGenerator.cs
@@ -9,6 +9,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 
+// ReSharper disable once CheckNamespace
 namespace GhostServiceBuster;
 
 [Generator]
