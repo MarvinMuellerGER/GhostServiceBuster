79308e952f1fe23e8aceba9df87fb46ad6471060
MarvinMuellerGER <113536472+MarvinMuellerGER@users.noreply.github.com>
2026-02-18 01:25:58 +0100

    add xml docs

--- src/Extensions/MS.Generator.Attributes/TypesResolvedByServiceProviderAttribute.cs
+++ src/Extensions/MS.Generator.Attributes/TypesResolvedByServiceProviderAttribute.cs
@@ -1,10 +1,10 @@
 namespace GhostServiceBuster.MS.Generator;
 
-[AttributeUsage(AttributeTargets.Assembly)]
 /// <summary>
 /// Marks assemblies with types that are resolved directly by a service provider.
 /// </summary>
 /// <param name="types">The types resolved by the service provider.</param>
+[AttributeUsage(AttributeTargets.Assembly)]
 public sealed class TypesResolvedByServiceProviderAttribute(params Type[] types) : Attribute
 {
     internal Type[] Types { get; } = types;
